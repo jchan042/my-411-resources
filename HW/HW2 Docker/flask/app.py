46ff7a5f06069ac4407ace5779f87fae6d9f2b55
jchan042 <jchan042>
2025-02-11 23:50:17 -0500

    branch reqs

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, make_response
+import os
+from flask import Flask, make_response, request
 
 app = Flask(__name__)
+
+keyword = 'PORT'
+portkey = os.getenv(keyword)
+
+thread = True
 
 @app.route('/')
 def hello():
@@ -12,8 +18,34 @@ def hello():
     )
     return response
 
+@app.route('/repeat', methods = ['GET'])
+def foo():
+    input = request.args.get('input')
+    return make_response(
+        {
+            'body': input,
+            'status': 200
+        
+         }
+     )
+
+@app.route('/health')
+@app.route('/healthcheck')
+def health():
+    response = make_response(
+        {
+            'body': 'OK',
+            'status': 200
+        }
+    )
+    return response
+
+@app.route('/hang')
+def break_app():
+    thread = False
+
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=portkey, debug=False, threaded=thread)
